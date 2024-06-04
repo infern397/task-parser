6fdfc5838ef1fbfeae84448b455127333a12de06
infern <semenmochalov55@gmail.com>
2024-06-04 14:10:33 +0300

    Update readme

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('fetch:incomes')->twiceDaily();
+        $schedule->command('fetch:all')->twiceDaily();
     }
 
     /**
